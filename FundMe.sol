475ceef22025086d105c140380974f0418a26f00
kxzb-fun <sunday_ever@163.com>
2024-06-22 09:46:48 +0800

    init program

---
+++ FundMe.sol
@@ -0,0 +1,17 @@
+// Get funds from users
+// Withdraw funds
+// Set a minmum fundinng value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    // allow user to send $
+    // have a minimun $ send
+
+    function fund() public {}
+
+    // function withdraw() public {
+
+    // }
+}
